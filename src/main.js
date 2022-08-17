8a1bdc277c64093b3e65aba768e80b997e3e1004
Kennedy <nofuequequise@gmail.com>
2022-08-17 17:02:09 -0400

    Cap7

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    }
+});
+
 async function getTrendingMoviesPreview(){
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY); // llamamos la url de la api
-    const data = await res.json(); // La convertimos a JSON
+    const {data} = await api('trending/movie/day'); // llamamos la url de la api
     const movies = data.results; //obtenemos los datos, en este caso los datos con la propiedad results
 
     movies.forEach(movie => {
@@ -18,8 +27,7 @@ async function getTrendingMoviesPreview(){
 }
 
 async function getCategoriesPreview(){
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY); // llamamos la url de la api
-    const data = await res.json(); // La convertimos a JSON
+    const{data} = await api('genre/movie/list');
     const categories = data.genres;
 
     categories.forEach(category => {
